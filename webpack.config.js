9b58087d1edc0aa5ff75699271781c83be098dfd
Nick Freeland <nfreeland@zendesk.com>
2017-03-15 15:34:42 +1100

    Use Garden instead of previous styling

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,10 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var externalAssets = {
   css: [
-    'https://cdn.jsdelivr.net/bootstrap/2.3.2/css/bootstrap.min.css'
+    'https://assets.zendesk.com/apps/sdk-assets/css/0/zendesk_garden.css'
   ],
   js: [
-    'https://cdn.jsdelivr.net/g/lodash@2.4.2(lodash.underscore.min.js),handlebarsjs@1.3.0,jquery@2.2.4,momentjs@2.9.0,bootstrap@2.3.2',
+    'https://cdn.jsdelivr.net/g/lodash@2.4.2(lodash.underscore.min.js),handlebarsjs@1.3.0,jquery@3.1.0,momentjs@2.9.0,bootstrap@2.3.2',
     'https://assets.zendesk.com/apps/sdk/2.0/zaf_sdk.js'
   ]
 }
